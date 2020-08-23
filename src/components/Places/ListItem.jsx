3ede58c91d1ed3aea99f555880fcd02e0bb7ecc1
Daniel <daniel.ortiz@nuu.co>
2020-08-23 13:46:33 -0500

    Fix images in Places list

--- src/components/Places/ListItem.jsx
+++ src/components/Places/ListItem.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 const ListItem = ({ id, name, slug, image, address, urlPrefix = '' }) => (
   <div role="listitem" className="col-4 mb-6 col-md-6 col-xs-12 w-dyn-item w-col w-col-4">
     <a data-w-id={id} href={`${urlPrefix}${slug}`} className="text-decoration-none w-block places-list-item">
-      <div className="place-image-container">
-        <img src={image} alt="" className="full-image" />
-      </div>
+      <div className="place-image-container" style={{ backgroundImage: image }}></div>
       <p className="normal-text text-navy">{name}</p>
       <p className="eyebrow-small text-steel text-uppercase">{address}</p>
     </a>
