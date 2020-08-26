42094aa09e16f44843b22c98733ee650eba5b0c7
Daniel <daniel.ortiz@nuu.co>
2020-08-25 20:23:44 -0500

    News and press respond to url

--- src/apps/NewsAndPress.js
+++ src/apps/NewsAndPress.js
@@ -1,4 +1,6 @@
-/* global CURRENT_SECTION:false, news_news:false publications_reports:false publications_magazine:false */
+/* global CURRENT_SECTION:false, news_news:false publications_reports:false
+    publications_magazine:false NEWS_URL:false PUBLICATIONS_URL:false
+    STORIES_URL:false AWARDS_URL:false */
 import React from 'react';
 import classnames from 'classnames';
 import { t as typy } from 'typy';
@@ -12,6 +14,30 @@ import '../css/NewsAndPress.css';
 
 function App() {
   const [section, setSection] = React.useState(CURRENT_SECTION);
+
+  const onChangeSection = (newSection) => {
+    setSection(newSection);
+
+    switch (newSection) {
+      case NNP_NEWS:
+        window.history.pushState(NNP_NEWS, 'News', NEWS_URL);
+        break;
+      case NNP_PUBS:
+        window.history.pushState(NNP_PUBS, 'Publications', PUBLICATIONS_URL);
+        break;
+      default:
+        break;
+    }
+  }
+
+  React.useEffect(() => {
+    window.onpopstate = function(e){
+      if (e.state){
+        setSection(e.state);
+      }
+    };
+  }, [])
+
   let children = null;
   switch (section) {
     case NNP_NEWS:
@@ -35,19 +61,19 @@ function App() {
       <Portal id='react-news-and-press-tabs' className='news-tabs'>
         <>
           <div className="mr-4">
-            <div onClick={() => setSection(NNP_NEWS)} className={classnames("tab-link-news", "cursor-pointer", { "w--current": section === NNP_NEWS })}>News &amp; Press</div>
+            <div onClick={() => onChangeSection(NNP_NEWS)} className={classnames("tab-link-news", "cursor-pointer", { "w--current": section === NNP_NEWS })}>News &amp; Press</div>
             <div className={classnames("hr-full-1px", { "bg-orange": section === NNP_NEWS })}></div>
           </div>
           <div className="mr-4">
-            <div onClick={() => setSection(NNP_PUBS)} className={classnames("tab-link-news", "cursor-pointer", { "w--current": section === NNP_PUBS })}>Publications</div>
+            <div onClick={() => onChangeSection(NNP_PUBS)} className={classnames("tab-link-news", "cursor-pointer", { "w--current": section === NNP_PUBS })}>Publications</div>
             <div className={classnames("hr-full-1px", { "bg-orange": section === NNP_PUBS })}></div>
           </div>
           <div className="mr-4">
-            <a href="/news/stories" className={classnames("tab-link-news", { "w--current": section === NNP_STOR })}>Stories</a>
+            <a href={STORIES_URL} className={classnames("tab-link-news", { "w--current": section === NNP_STOR })}>Stories</a>
             <div className={classnames("hr-full-1px", { "bg-orange": section === NNP_STOR })}></div>
           </div>
           <div>
-            <a href="/news/awards" className={classnames("tab-link-news", { "w--current": section === NNP_AWRD })}>Awards</a>
+            <a href={AWARDS_URL} className={classnames("tab-link-news", { "w--current": section === NNP_AWRD })}>Awards</a>
             <div className={classnames("hr-full-1px", { "bg-orange": section === NNP_AWRD })}></div>
           </div>
         </>
